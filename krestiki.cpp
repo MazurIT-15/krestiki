cb1d1a209a4ef0ba7459cea06b7c6e07d043c8cf
MazurIT-15 <mazursofia89@yandex.ru>
2024-12-10 21:07:07 +0500

    Добавлена функция вывода массива

--- krestiki.cpp
+++ krestiki.cpp
@@ -3,13 +3,25 @@
 
 using namespace std;
 
+void print(char mas[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << mas[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
     char mas[3][3];
 
-    for (int i = 0; i < 3; i++) //заполняем массив
+    for (int i = 0; i < 3; i++) // заполнение массива
     {
         for (int j = 0; j < 3; j++)
         {
